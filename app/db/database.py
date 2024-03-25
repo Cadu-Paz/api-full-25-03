b6c1e9f0ac3575e034cd6be7a9169447d7bddd45
Carlos Eduardo Corberi Paz <2203078@SALAS.AULAS>
2024-03-25 11:46:48 -0300

    meu primeiro comit

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import declarative_base
+
 from sqlalchemy import create_engine
 import sqlalchemy.orm
 from db.config import settings
@@ -15,6 +17,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 def get_db():
